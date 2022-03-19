c52aba915d2685795cfe6dce8291e9dc91e6da94
panar <pan.arno@gmail.com>
2022-03-19 15:59:24 -0700

    Attempted login

--- app/src/main/java/com/bcit/titan/MainActivity.java
+++ app/src/main/java/com/bcit/titan/MainActivity.java
@@ -1,20 +1,117 @@
 package com.bcit.titan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // hide ActionBar
         getSupportActionBar().hide();
+
+//        db = FirebaseFirestore.getInstance();
+
+
+        View login = findViewById(R.id.button_bottomNav_login);
+        Intent intent = new Intent(this, HomeActivity.class);
+        mAuth=FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userName = findViewById(R.id.editText_main_username);
+                EditText password = findViewById(R.id.editText_main_password);
+
+                String userNameStr = userName.getText().toString();
+                String passwordStr = password.getText().toString();
+
+                System.out.println("The username: " + userName.getText().toString());
+                System.out.println("The password: " + password.getText().toString());
+
+/*                List<User> user = new ArrayList<User>();
+
+                db.collection("users")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("Debug", document.getData().toString());
+
+                                        user.add(
+                                                new User(
+                                                        document.getData().get("username").toString(),
+                                                        document.getData().get("password").toString(),
+                                                        document.getData().get("firstName").toString(),
+                                                        document.getData().get("lastName").toString(),
+                                                        document.getData().get("email").toString()
+                                                )
+                                        );
+                                        System.out.println("List: " + user);
+                                        for (int i = 0; i < user.size(); i++) {
+                                            System.out.println("Firebase User: " + user.get(i).getUsername());
+                                            System.out.println("Firebase User: " + user.get(i).getPassword());
+                                        }
+
+                                    }
+                                } else {
+                                    Log.w("Debug", "Error getting documents", task.getException());
+                                }
+
+                                User[] usersArray = user.toArray(new User[user.size()]);
+                            }
+                        });*/
+
+                mAuth.signInWithEmailAndPassword(userNameStr, passwordStr).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                    }
+
+                });
+
+
+
+
+            }
+
+        });
 
 
     }
--- app/src/main/java/com/bcit/titan/SignUpActivity.java
+++ app/src/main/java/com/bcit/titan/SignUpActivity.java
@@ -1,16 +1,56 @@
 package com.bcit.titan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        db = FirebaseFirestore.getInstance();
+
+
     }
 
+    public void addPerson(View view){
+        EditText userName = findViewById(R.id.editText_signup_username);
+        EditText password = findViewById(R.id.editText_signup_password);
+        EditText firstName = findViewById(R.id.editText_signup_first_name);
+        EditText lastName = findViewById(R.id.editText_signup_last_name);
+        EditText email = findViewById(R.id.editText_signup_email);
+        User user = new User(userName.getText().toString(), password.getText().toString(), firstName.getText().toString(), lastName.getText().toString(), email.getText().toString());
+        db.collection("users")
+                .add(user) // user
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Debug", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Intent intent = new Intent(getBaseContext(), SignUpActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Debug", "Error adding document", e);
+                    }
+                });
+    }
 
 }
